81ed1ae821c437f3a8763e5d916763a61d94d3ba
Peter Conley <peterdavidconley@gmail.com>
2022-02-10 14:46:51 -0500

    wrote POST endpoints

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ require('dotenv').config()
 
 const PORT = process.env.PORT;
 
+// Functions
+
+function insert(post) {
+    return db('posts')
+      .insert(post)
+      .then(ids => ({ id: ids[0] }));
+  }
+
+  // Endpoints
+
 server.listen(PORT, () => {
     console.log(`***** Listening on port ${PORT} *****`)
 })
@@ -26,8 +36,23 @@ server.get('/api/users', (req, res) => {
 
 server.post('/api/register', (req, res) => {
 
+    insert(req.body.params)
+    .then(user => {
+        res.status(201).json(user)
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Error happened registering.'})
+    })
+
 })
 
 server.post('/api/login', (req, res) => {
         
+    insert(req.body.params)
+    .then(user => {
+        res.status(201).json(user)
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Error happened logging in.'})
+    })
 })
